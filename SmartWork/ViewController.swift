959591f386161e20482844d3199f69d1a79672e8
nico <529725067@qq.com>
2017-05-30 12:13:03 +0800

    Initial Commit

---
+++ SmartWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartWork
+//
+//  Created by apple on 2017/5/30.
+//  Copyright © 2017年 NicoDeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
