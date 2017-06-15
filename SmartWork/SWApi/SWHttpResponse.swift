f0d92c2e1888820c7ae70371d535b011f2846b17
nico <529725067@qq.com>
2017-06-15 21:13:02 +0800

    网络请求新建类,待完善

---
+++ SmartWork/SWApi/SWHttpResponse.swift
@@ -0,0 +1,13 @@
+//
+//  SWHttpResponse.swift
+//  SmartWork
+//
+//  Created by apple on 2017/6/11.
+//  Copyright © 2017年 NicoDeng. All rights reserved.
+//
+
+import UIKit
+
+class SWHttpResponse: NSObject {
+
+}
