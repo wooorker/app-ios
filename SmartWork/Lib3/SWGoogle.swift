086d7d966ca72bb7bc1dfcd7311db5df70dbb665
nico <529725067@qq.com>
2017-08-13 21:46:01 +0800

    SWGoogle segment

---
+++ SmartWork/Lib3/SWGoogle.swift
@@ -0,0 +1,13 @@
+//
+//  SWGoogle.swift
+//  SmartWork
+//
+//  Created by apple on 2017/8/13.
+//  Copyright © 2017年 NicoDeng. All rights reserved.
+//
+
+import UIKit
+
+class SWGoogle: NSObject {
+
+}
